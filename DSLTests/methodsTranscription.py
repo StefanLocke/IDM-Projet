0ca0a6bd3d2af08b527c58de6d508e11094b1cdc
Alexis Leloup <alexleloup@yahoo.fr>
2021-12-16 16:57:45 +0100

    add description for python

---
+++ DSLTests/methodsTranscription.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# Create dataframe
+# df = pd.DataFrame()
+
+# Load csv as dataframe
+# df = pd.read_csv("CsvTestFile.csv")
+
+# Insert element in specific column
+# df.insert(int:ROW, string:COLUMN_NAME, VALUE)
+# example : df.insert(0, "Article", "")
+
+# Insert element in specific column with id
+# df.insert(int:ROW, df.columns[int:POSITION], VALUE)
+# example : df.insert(0, "Article", "")
+
+# Insert empty line at end
+# df.loc[df.size] = [None] * df.columns.size
+
+# Insert empty line at specific position
+# df.loc[POSITION] = [None] * df.columns.size
+
+# Remove Col with col name
+# df = df.drop(columns=NAME)
+
+# Remove Col with col id
+# df = df.drop(columns=df.columns[ID])
+
+# Remove line
+# df = df.drop(POSITION)
