95e25d3238e5dbd287129097b34e6994fcd267c8
Leloup <alexleloup@yahoo.fr>
2022-02-21 12:40:58 +0100

    Update test comparision method

--- eclipse/idm.project.tests/src/org/xtext/example/mydsl/tests/TestUtils.java
+++ eclipse/idm.project.tests/src/org/xtext/example/mydsl/tests/TestUtils.java
@@ -1,6 +1,9 @@
 package org.xtext.example.mydsl.tests;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -139,7 +142,7 @@ public class TestUtils {
 	 * @return true if similar else false
 	 * @throws IOException
 	 */
-	public Boolean compareFiles(String filePath1, String filePath2) throws IOException{
+	public Boolean compareFiles2(String filePath1, String filePath2) throws IOException{
 		System.out.println("Compare " + filePath1 + " and " + filePath2);
         var first = Files.readAllBytes(Paths.get(filePath1));
         var second = Files.readAllBytes(Paths.get(filePath2));
@@ -153,8 +156,49 @@ public class TestUtils {
         return r;
    }
 	
+	public Boolean compareFiles(String filePath1, String filePath2) throws IOException{
+		
+		System.out.println("Compare " + filePath1 + " and " + filePath2);
+		
+		BufferedReader reader1 = new BufferedReader(new FileReader(filePath1));
+        BufferedReader reader2 = new BufferedReader(new FileReader(filePath2));
+         
+        String line1 = reader1.readLine();
+        String line2 = reader2.readLine();
+         
+        boolean areEqual = true;
+         
+        int lineNum = 1;
 
-	
-	
+        while (line1 != null || line2 != null){
+        	
+            if(line1 == null || line2 == null){
+                areEqual = false;
+                break;
+            }
+            else if(!line1.strip().equals( line2.strip())){
+                areEqual = false;
+                break;
+            }
+             
+            line1 = reader1.readLine(); 
+            line2 = reader2.readLine();
+             
+            lineNum++;
+        }
+         
+        if(areEqual){
+            System.out.println("Files are similar");
+        }
+        else{
+            System.out.println("Files are different at line "+lineNum);
+            System.out.println("File1='"+line1+"'\nFile2='"+line2+"'");
+        }
+         
+        reader1.close();
+        reader2.close();
+        
+        return areEqual;
+    }
 
 }
