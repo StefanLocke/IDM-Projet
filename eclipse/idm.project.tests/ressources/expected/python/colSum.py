0c4f93a3f73ca0d4b42ec192cb7a0fa01278de16
Nusky173 <simon50.lecordier@gmail.com>
2022-02-21 22:28:29 +0100

    col sum expected

--- eclipse/idm.project.tests/ressources/expected/python/colSum.py
+++ eclipse/idm.project.tests/ressources/expected/python/colSum.py
@@ -1,6 +1,6 @@
 import pandas as pd
 df = pd.read_csv( "/home/simonlecordier/workspace/m1/IDM/IDM-Projet/eclipse/idm.project.tests/ressources/input/fruits.csv" )
-df.append(list(), ignore_index=True)
 df.at[6, "Article"] = "Benefice Total"
-df.at[6, "Benefice"] = df.sum(axis=0)["Benefice"]
+df.at[6, "Stock"] = df.sum(axis=0)["Benefice"]
 df.to_csv("/home/simonlecordier/workspace/m1/IDM/IDM-Projet/eclipse/idm.project.tests/ressources/output/colSumPython.csv", index=False)
+
