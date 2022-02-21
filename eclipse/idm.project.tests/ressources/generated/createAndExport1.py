d276b66a66f9924089d82b60007554301b82ff22
Leloup <alexleloup@yahoo.fr>
2022-02-21 20:15:44 +0100

    Update Ressources

--- eclipse/idm.project.tests/ressources/generated/createAndExport1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.DataFrame(list())
-df.insert(0,"Prenom","")
-df.insert(1,"Sexe","")
-df.at[0, "Prenom"] = "Alexis"
-df.at[0, "Sexe"] = "Male"
-df.to_csv("C:/Users/Leloup/Documents/FAC/M2/IDM/Git/IDM-Projet/eclipse/idm.project.tests/ressources/output/createAndExport1Python.csv", index=False)
-
--- eclipse/idm.project.tests/ressources/generated/loadAndExport.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv( "C:/Users/Leloup/Documents/FAC/M2/IDM/Git/IDM-Projet/eclipse/idm.project.tests/ressources/input/fruits.csv" )
-df.to_csv("C:/Users/Leloup/Documents/FAC/M2/IDM/Git/IDM-Projet/eclipse/idm.project.tests/ressources/output/loadAndExportPython.csv", index=False)
-
