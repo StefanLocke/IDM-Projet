5af65b0077c9d2546ac1f5ac7598e02a4f91292f
Leloup <alexleloup@yahoo.fr>
2022-02-21 11:37:43 +0100

    Update Tests

--- eclipse/idm.project.tests/src/org/xtext/example/mydsl/tests/PythonCompilerTest.xtend
+++ eclipse/idm.project.tests/src/org/xtext/example/mydsl/tests/PythonCompilerTest.xtend
@@ -16,36 +16,55 @@ class PythonCompilerTest {
 	ParseHelper<Programme> parseHelper;
 	
 	TestUtils testUtils = new TestUtils();
-	String inputFilePath = testUtils.getInputPath + "/recettesFruits.csv"
+	String inputFilePath = testUtils.getInputPath + "/fruits.csv"
 	
+	String ext = ".py";
 	
     
     @Test
     def void test1() {
-    	var currentTest = "test1"
     	
+    	// Test Name
+    	var testName = "test1"
+    	
+    	// Start Time
+    	var startTime = System.nanoTime();
+    	
+    	// Parse Instructions
         val result = parseHelper.parse('''
             Load('«inputFilePath»') {
-                Store('«testUtils.getOutputPath + "/" + currentTest + ".csv"»');
+                Store('«testUtils.getOutputPythonTestPath(testName)»');
             }
         ''') 
         
+        // Assert parse works
+        Assertions.assertNotNull(result)
+        
+        // Initialize compiler and get result
         val compiler = new PythonCompiler(result);
         var compilerResult = compiler.doCompile
+        
+        // Elapsed time
+        var timeElapsed = System.nanoTime() - startTime;
+        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
+        
+        println("Compiler result :")
         println(compilerResult)
         
-        testUtils.writeFile(testUtils.getGeneratedPath + "/" + currentTest + ".py", compilerResult)
+        // Assert there is no errors during compilation
+        Assertions.assertTrue(result.eResource.errors.isEmpty)
         
+        // Get path of generated
+        var generated_file_path = testUtils.getGeneratedPythonTestPath(testName);
         
-        Assertions.assertNotNull(result)
-        val errors = result.eResource.errors
-        //Assertions.assertTrue(errors.isEmpty, '''Unexpected errors: «errors.join(", ")»''')
+        // Write compiler result as python file
+        testUtils.writeFile(generated_file_path, compilerResult)
         
+		// Execute python file
+        testUtils.runPython(generated_file_path, "python")
         
-        testUtils.runPython(testUtils.getGeneratedPath + "/" + currentTest + ".py", "python")
-        
-
-        
+        // Compare generated and expected
+        Assertions.assertTrue(testUtils.compareFiles(testUtils.getOutputPythonTestPath(testName), testUtils.getExpectedTestPath(testName)))
     } 
     
 }
--- eclipse/idm.project.tests/src/org/xtext/example/mydsl/tests/TestUtils.java
+++ eclipse/idm.project.tests/src/org/xtext/example/mydsl/tests/TestUtils.java
@@ -12,36 +12,115 @@ public class TestUtils {
 	String inputPath = (System.getProperty("user.dir") + "/ressources/input").replace("\\", "/");
 	String outputPath = (System.getProperty("user.dir") + "/ressources/output").replace("\\", "/");
 	String generatedPath = (System.getProperty("user.dir") + "/ressources/generated").replace("\\", "/");
+	String expectedPath = (System.getProperty("user.dir") + "/ressources/expected").replace("\\", "/");
 	
 	public String getInputPath() {
 		return inputPath;
 	}
+	
 	public String getOutputPath() {
 		return outputPath;
 	}
+	
 	public String getGeneratedPath() {
 		return generatedPath;
 	}
 	
-	public void writeFile(String filePath, String fileContent) throws IOException{
-		try {
-	        System.out.println("Write " + filePath);
-	        var writer = new FileWriter(new File(filePath));
-	        writer.write(fileContent);
-	        writer.flush();
-	        writer.close();
-		}catch(Exception e) {
-	        System.out.println("Exception Raised" + e.toString());
-	     }
+	public String getExpectedPath() {
+		return expectedPath;
 	}
 	
+	/**
+	 * Get expected test file path for a test
+	 * @param testName 
+	 * @return expected path for test
+	 */
+	public String getExpectedTestPath(String testName) {
+		return expectedPath + "/" + testName + ".csv";
+	}
+	
+	/**
+	 * Get generated file path for Python test
+	 * @param testName
+	 * @return generated Python file path
+	 */
+	public String getGeneratedPythonTestPath(String testName) {
+		return generatedPath + "/" + testName + ".py";
+	}
+	
+	/**
+	 * Get generated file path for R test
+	 * @param testName
+	 * @return generated R file path
+	 */
+	public String getGeneratedRTestPath(String testName) {
+		return generatedPath + "/" + testName + ".r";
+	}
+	
+	/**
+	 * Get output .csv file path for a Python test
+	 * @param testName
+	 * @return output .csv file path
+	 */
+	public String getOutputPythonTestPath(String testName) {
+		return outputPath + "/" + testName + "Python.csv";
+	}
+	
+	/**
+	 * Get output .csv file path for a R test
+	 * @param testName
+	 * @return output .csv file path
+	 */
+	public String getOutputRTestPath(String testName) {
+		return outputPath + "/" + testName + "R.csv";
+	}
+	
+	
+	/**
+	 * Write a file
+	 * @param filePath file to write
+	 * @param fileContent file content to write
+	 * @throws IOException
+	 */
+	public void writeFile(String filePath, String fileContent) throws IOException{
+        System.out.println("Write " + filePath);
+        var writer = new FileWriter(new File(filePath));
+        writer.write(fileContent);
+        writer.flush();
+        writer.close();
+	}
+	
+	/**
+	 * Run R Python from command line
+	 * @param pythonFilePath path of Python file to run
+	 * @param pythonModuleName name of Python module in current command line prompt
+	 * @throws IOException
+	 */
 	public void runPython(String pythonFilePath, String pythonModuleName) throws IOException{
 		var rt = Runtime.getRuntime();
 		var pr = rt.exec(pythonModuleName + " " + pythonFilePath);
-
 	}
 	
+	/**
+	 * Run R script from command line
+	 * @param rFilePath path of r file to run
+	 * @param rModuleName name of r module in current command line prompt
+	 * @throws IOException
+	 */
+	public void runR(String rFilePath, String rModuleName) throws IOException{
+		var rt = Runtime.getRuntime();
+		var pr = rt.exec(rModuleName + " " + rFilePath);
+	}
+	
+	/**
+	 * Compare two files
+	 * @param filePath1 path of first file
+	 * @param filePath2 path of second file
+	 * @return true if similar else false
+	 * @throws IOException
+	 */
 	public Boolean compareFiles(String filePath1, String filePath2) throws IOException{
+		System.out.println("Compare " + filePath1 + " and " + filePath2);
         var first = Files.readAllBytes(Paths.get(filePath1));
         var second = Files.readAllBytes(Paths.get(filePath2));
         return Arrays.equals(first, second);
